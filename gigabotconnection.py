3edc532a7fa14976af54576d9dcda8bebc0bb852
plloppii <noahpan323@gmail.com>
2019-07-01 08:20:00 -0700

    Client code edited code to extract upload date code to extract Gigabot model :

--- gigabotconnection.py
+++ gigabotconnection.py
@@ -6,19 +6,40 @@ import serial.tools.list_ports
 class gcodedata:
 	def __init__(self):
 		self.temp = dict()
+		self.uploaddate= ""
+		self.model = ""
 
 	def extracttemp(self,variable,data):
+		#Extracting temperature data to a tuple in the format (Temp/SetTemp)
 		if variable in data:
-			start= end= data.find(variable) + len(variable)
-			while((ord(data[end])< 58 and ord(data[end])>45) or ord(data[end]) == 32): end+=1
+			start = end = data.find(variable) + len(variable)
+			while((ord(data[end]) < 58 and ord(data[end])>45) or ord(data[end]) == 32): end+=1
 			tup = [float(x) for x in data[start:end].split("/")]
 			self.temp[variable[:-1]] = tup
+	def extractheader(self, data):
+		#Searching for last updated timestamp
+		start = end = data.find("Last Updated:") + len("Last Updated:")
+		while(ord(data[end]) != 124): end += 1
+		self.uploaddate = data[start:end].strip()
+		print(self.uploaddate)
+		start = end
+		#Increment until you hit "G" for GB
+		while(ord(data[start]) != 66): start += 1
+		end = start
+		while(ord(data[end]) != 86): end += 1
+		modstring = data[start+1:end].strip()
+		if("3" in modstring): self.model = "Regular"
+		else: self.model = modstring
+		print self.model
 
-	def parsedata(self,data):
-		if('T' in data):
+
+	def parsedata(self,msglen, data):
+		if(msglen <200 and 'T' in data):
 			self.extracttemp("T0:", data)
 			self.extracttemp("T1:", data)
 			self.extracttemp("B:", data)
+		if(msglen >200 and "Updated" in data):
+			self.extractheader(data)
 
 
 # if __name__ == "__main__":
--- gigabotnode.py
+++ gigabotnode.py
@@ -10,6 +10,20 @@ host = '192.168.1.49'
 baudrate = 250000
 port = 63200
 
+def connecttocom():
+	com = list(serial.tools.list_ports.comports())
+	for p in com:
+		print p.device
+		if "/dev/ttyUSB" in p.device:
+			com = p.device
+	try:
+		#open up serial port.
+		serconn = serial.Serial(com, baudrate=baudrate)
+		return serconn
+	except:
+		print "COM port is unavalible/ or run program with root permission."
+		exit()
+
 
 if __name__ == "__main__":
 	#connect to the server
@@ -21,36 +35,33 @@ if __name__ == "__main__":
 	print(data)
 
 
-	com = list(serial.tools.list_ports.comports())
-	for p in com:
-		print p.device
-		if "/dev/ttyUSB" in p.device:
-			com = p.device
-	try:
-		#open up serial port.
-		serialconn = serial.Serial(com, baudrate=baudrate)
-	except:
-		print "COM port is unavalible/ or run program with root permission."
-		exit()
+	serialconn = connecttocom() #connect to the serial device
 
 	serialconn.timeout = 1  # set read timeout
+
 	if serialconn.is_open:
 		time.sleep(1)
 		print("SEND: M155 S5\r")
 		serialconn.write('M155 S5\r'.encode('utf-8'))
 		gcodedata = gigabotconnection.gcodedata()
 		while True:
-			insize = serialconn.inWaiting()
-			if insize:
-				#Once recieved data, wait 50ms for the rest of the data to come in.
-				time.sleep(0.05)
+			try:
 				insize = serialconn.inWaiting()
-				print(insize)
-			 	serial_recv = serialconn.read(insize)
-			 	print(serial_recv)
-			 	d_serial_recv = serial_recv.decode('utf-8')
-			 	gcodedata.parsedata(d_serial_recv)
-			 	print(gcodedata.temp)
-				#conn.send(giga.temp.encode())
+				if insize:
+					#Once recieved data, wait 50ms for the rest of the data to come in.
+					time.sleep(0.05)
+					insize = serialconn.inWaiting()
+					
+					print(insize)
+				 	serial_recv = serialconn.read(insize)
+				 	print(serial_recv)
+				 	d_serial_recv = serial_recv.decode('utf-8')
+				 	gcodedata.parsedata(insize, d_serial_recv)
+				 	print(gcodedata.temp)
+					#conn.send(giga.temp.encode())
+			except IOError:
+				print "Device Disconnected!\n"
+				break
+	print "program ended"
 
 	# conn.close()
