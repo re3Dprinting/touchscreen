d28b30fcd339b30e7c0ff6a924a9f6d33aba6edf
plloppii <noahpan323@gmail.com>
2019-07-03 15:26:10 -0700

    Serial class updated, inherited Serial and Socket, class structure reorganized

--- gigabotnode.py
+++ gigabotnode.py
@@ -2,88 +2,61 @@ from socket import *
 import time
 import serial
 import serial.tools.list_ports
-import gigabotconnection
+import g_serial
 import sys
 import json
 
-host = '192.168.1.49'
 baudrate = 250000
-port = 63200
 
-def connecttocom():
-	com = list(serial.tools.list_ports.comports())
-	for p in com:
-		print p.device
-		if "/dev/ttyUSB" in p.device:
-			com = p.device
-	try:
-		#open up serial port.
-		serconn = serial.Serial(com, baudrate=baudrate)
-		serconn.timeout = 1 # set serial read timeout
-	except:
-		print "COM port is unavalible/ or run program with root permission."
-		exit()
-	#Toggle Arduino DTR pin to restart the firmware
-	serconn.setDTR(False)
-	time.sleep(1)
-	serconn.flushInput()
-	serconn.setDTR(True)	
-	return serconn
+class rpiclient(socket):
+	def __init__(self, host, port):
+		socket.__init__(self,AF_INET,SOCK_STREAM)
+		self.connect((host,port))
+		self.serial = None
+		data = self.recv(1024)
+		
+		self.waitforACK()
 
-def extractheader(client, conn, serdata):
-	insiz = conn.inWaiting()
-	if insiz: time.sleep(0.5)
-	insiz = conn.inWaiting()
-	print(insiz)
-	serdata.parsedata(insiz, conn.read(insiz).decode("utf-8"))
-	t = json.dumps(serdata.uploaddate +"|"+ serdata.model).encode("base64")
-	client.send(t)
+		self.send("Client Confirmed Connection\n")
+	def senddata(self, msg):
+		if msg:
+			temp = json.dumps(msg).encode("base64")
+			self.send(temp)
+	def waitforACK(self):
+		data = self.recv(1024)
+		while(len(data) == 0):
+			data = self.recv(1024)
+
 
 
 if __name__ == "__main__":
 	#connect to the server
-	gigabotclient= socket(AF_INET,SOCK_STREAM)
-	gigabotclient.connect((host,port))
-	data = gigabotclient.recv(1024)
-	while(len(data) == 0): #Wait for the Connected transmission
-		data = gigabotclient.recv(1024)
-	print(data)
-	gigabotclient.send("Connection Confirmed\n")
-	
+	g_client= rpiclient("192.168.1.49",63200)
 
-	#connect to the serial device
-	serialconn = connecttocom() 
-	if serialconn.is_open:
+
+	serial_conn = g_serial.serialconn()
+
+	#Send server a notification that client is waiting for Gigabot connection.
+	#if not serial_conn.is_open: g_client
+	serial_conn.attemptconnection()
+
+	#Extract Header information from the first few bytes of data
+	header = serial_conn.readdata()
+	g_client.senddata(header)
+	g_client.waitforACK()
+
+	serial_conn.connect_to_bot()
+
+	while True:
 		time.sleep(1)
-		print("SEND: M155 S5\r")
-		#send to serial a M155 code to enable temperture reportings every 5s
-		serialconn.write('M155 S5\r'.encode('utf-8'))
-		time.sleep(1)
-		#create new serial data object. 
-		serial_data = gigabotconnection.serialdata()
-
-		extractheader(gigabotclient, serialconn, serial_data)
-
-		while True:
-			try:
-				insize = serialconn.inWaiting()
-				if insize:
-					#Once recieved data, wait 50ms for the rest of the data to come in.
-					time.sleep(0.05)
-					insize = serialconn.inWaiting()
-					
-					print(insize)
-				 	d_serial_recv = serialconn.read(insize).decode('utf-8')
-				 	print d_serial_recv
-					serial_data.parsedata(insize, d_serial_recv)
-					
-					if(len(serial_data.temp) != 0) :
-						t = json.dumps(serial_data.temp).encode("base64")
-				 		print(serial_data.temp)
-						gigabotclient.send(t)
-			except IOError:
-				print "Device Disconnected!\n"
-				break
+		try:
+			d = serial_conn.readdata()
+			g_client.senddata(d)
+			g_client.waitforACK()
+			
+		except IOError:
+			print "Device Disconnected!\n"
+			break
 	print "program ended"
 
 	# conn.close()
