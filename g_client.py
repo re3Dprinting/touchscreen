d3c84cb5733760ef7143b1e53ba5555967689484
plloppii <noahpan323@gmail.com>
2019-07-08 13:44:44 -0500

    client class seperated from main class, mainhandler class added as a overhead for the serial and socket classes

---
+++ g_client.py
@@ -0,0 +1,35 @@
+
+class g_client(socket):
+	def __init__(self, host, port):
+		try:
+			socket.__init__(self,AF_INET,SOCK_STREAM)
+			self.connect((host,port))
+			self.serial = None
+			#First packet is Server Confirmation 
+			confirm = self.recvdata()
+			print confirm
+			#Send Connection Confirmation
+			self.senddata("Client Confirmed Connection\n")
+			self.is_conn = True
+		except error, exc:
+			print "Error Connecting to Server: ", exc
+			self.is_conn = False
+
+	def attemptconnect(self, host, port):
+		self.__init__(host, port)
+
+	#senddata function converts data into json string then encodes it
+	#Data is sent if the msg is not 0
+	def senddata(self, msg):
+		if msg:
+			temp = json.dumps(msg).encode("base64")
+			self.send(temp)
+			self.recvdata() #Wait for ACK message
+	def recvdata(self):
+		data = self.recv(1024)
+		while(len(data) == 0):
+			data = self.recv(1024)
+		if data:
+			m_data = json.loads(data.decode("base64"))
+			if(m_data == "OK"): print "Server ACK"
+			else: return m_data
--- gigabotnode.py
+++ gigabotnode.py
@@ -3,6 +3,7 @@ import time
 import serial
 import serial.tools.list_ports
 import g_serial
+import g_client
 import sys
 import json
  
@@ -12,80 +13,60 @@ baudrate = 250000
 serverconnected = False
 serialconnected = False
 
-class rpiclient(socket):
-	def __init__(self, host, port):
+class mainhandler():
+	def __init__(self, clientconn, serialconn):
+		self.clientconn = clientconn
+		self.serialconn = serialconn
+	def attempt_transfer_header():
 		try:
-			socket.__init__(self,AF_INET,SOCK_STREAM)
-			self.connect((host,port))
-			self.serial = None
-			#First packet is Server Confirmation 
-			confirm = self.recvdata()
-			print confirm
-			#Send Connection Confirmation
-			self.senddata("Client Confirmed Connection\n")
-			self.is_conn = True
+			if client_conn.is_conn and serial_conn.is_open:
+				client_conn.senddata(serial_conn.header)
 		except error, exc:
-			print "Error Connecting to Server: ", exc
-			self.is_conn = False
-
-	def attemptconnect(self, host, port):
-		self.__init__(host, port)
-
-
-	#senddata function converts data into json string then encodes it
-	#Data is sent if the msg is not 0
-	def senddata(self, msg):
-		if msg:
-			temp = json.dumps(msg).encode("base64")
-			self.send(temp)
-			self.recvdata() #Wait for ACK message
-	def recvdata(self):
-		data = self.recv(1024)
-		while(len(data) == 0):
-			data = self.recv(1024)
-		if data:
-			m_data = json.loads(data.decode("base64"))
-			if(m_data == "OK"): print "Server ACK"
-			else: return m_data
-
-
+			client_conn.is_conn = False
+			print "Server Disconnected!"
+		except IOError:
+			serial_conn.is_open = False
+			print "Serial Disconnected!\n"
 
 if __name__ == "__main__":
 	#connect to the server
-	g_client= rpiclient("192.168.1.211",63200)
-
+	client_conn= g_client("192.168.1.211",63200)
 	serial_conn = g_serial.serialconn()
 
-	#Send server a notification that client is waiting for Gigabot seriall connection.
+	mainhand = mainhandler(client_conn, serial_conn)
 
 	#Send Header data if both serial and server are connected
-	if g_client.is_conn and serial_conn.is_open:
-		g_client.senddata(serial_conn.header)
+	mainhand.attempt_transfer_header()
 
 	while True:
 		time.sleep(1)
+		try:
+			#Conditional statement if one or both of the Server and Serial is disconnected.
+			if(not (client_conn.is_conn and serial_conn.is_open) ):
+				#If either one is disconnected, attempt to connect.
+				if not client_conn.is_conn: client_conn.attemptconnect("192.168.1.211",63200)
+				if not serial_conn.is_open: serial_conn.attemptconnection()
+				#The instant both become connected, send the header to the server.
+				if(client_conn.is_conn and serial_conn.is_open): glient._connsenddata(serial_conn.header)
 
-		#Conditional statement if one or both of the Server and Serial is disconnected.
-		if(not (g_client.is_conn and serial_conn.is_open) ):
-			#If either one is disconnected, attempt to connect.
-			if not g_client.is_conn: g_client.attemptconnect("192.168.1.211",63200)
-			if not serial_conn.is_open: serial_conn.attemptconnection()
+			if client_conn.is_conn and not serial_conn.is_open:
+				#Send OFF/Disconnected status to server
+				pass
 
-			#The instant both become connected, send the header to the server.
-			if(g_client.is_conn and serial_conn.is_open): 
-				g_client.senddata(serial_conn.header)
+			if not client_conn.is_conn and serial_conn.is_open:
 
-		if(serial_conn.is_open):
-			try:
 				d = serial_conn.readdata()
-				if g_client.is_conn: g_client.senddata(d)
 
-			except error, exc:
-				g_client.is_conn = False
-				print "Server Disconnected!"
-			except IOError:
-				serial_conn.is_open = False
-				print "Serial Disconnected!\n"
+			elif client_conn.is_conn and serial_conn.is_open:
+				d = serial_conn.readdata()
+				client_conn.senddata(d)
+		except error, exc:
+			client_conn.is_conn = False
+			print "Server Disconnected!"
+		except IOError:
+			serial_conn.is_open = False
+			print "Serial Disconnected!\n"
+
 	print "program ended"
 
 	# conn.close()
