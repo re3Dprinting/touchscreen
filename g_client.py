7ac4fa2d9c87c5dd80ff0610f891cbac21bf3b50
plloppii <noahpan323@gmail.com>
2019-07-19 11:14:38 -0500

    Code commented and reorganized

--- g_client.py
+++ g_client.py
@@ -1,6 +1,7 @@
 from socket import *
 import json
 
+#	The g_client class inherits from the socket class
 class g_client(socket):
 	def __init__(self, host, port):
 		try:
@@ -14,20 +15,26 @@ class g_client(socket):
 			self.senddata("Client Confirmed Connection\n")
 			self.is_conn = True
 
+		#TRY TO CONNECT AGAIN IN 5 SECONDS.
 		except error, exc:
 			print "Error Connecting to Server: ", exc
 			self.is_conn = False
 
+#	Attempt to reconnect to Server
 	def attemptconnect(self, host, port):
 		self.__init__(host, port)
 
-	#senddata function converts data into json string then encodes it
-	#Data is sent if the msg is not 0
+#	senddata function converts data into json string then encodes it
+#	If no data, send "None" msg to ping server. 
 	def senddata(self, msg):
 		if msg:
 			temp = json.dumps(msg).encode("base64")
 			self.send(temp)
 			self.recvdata() #Wait for ACK message
+
+#	recvdata waits for the server to acknowledge
+#	Should close connection if timeout
+#	IMPLEMENT TIMEOUT CODE HERE. SHOULD CLOSE IF TIMED OUT
 	def recvdata(self):
 		data = self.recv(1024)
 		while(len(data) == 0):
--- g_data.py
+++ g_data.py
@@ -1,28 +1,79 @@
 from socket import *
 import time
 
+#	g_data class is a data class that handles parsing data
+#	and a buffer that is periodically sent to the server
 class g_data:
 	def __init__(self):
 		self.temp = dict()
 		self.uploaddate= ""
 		self.model = ""
+		self.header = ""
 		self.status = ""
 		self.printtime = ""
 		self.currentfile = ""
 		self.stats = dict()
 		self.buffer = dict()
-		s = socket(AF_INET, SOCK_DGRAM)
-		s.connect(("8.8.8.8",80))
-		self.ipaddr = s.getsockname()[0]
-		s.close()
+		self.ipaddr = self.getipaddress()
 
-	def changestatus(self, stat):
-		self.status = stat
-		self.buffer["ST"] = stat
+#	Attempt to get the IP address through connecting to Google DNS to get current ipaddress
+	def getipaddress(self):
+		try:
+			s = socket(AF_INET, SOCK_DGRAM)
+			s.connect(("8.8.8.8",80))
+			ip = s.getsockname()[0]
+			s.close()
+			return ip
+		except error, exc:
+			return ""
 
+#	Add data to buffer to be sent through the SenderThread
 	def addtobuffer(self, item, val):
 		self.buffer[item] = val
 
+#	Main Data Handling Function
+	def parsedata(self, msglen, serialdata):
+		data_ = serialdata.decode("utf-8")
+		if( "M23" in data_ and "M24" in data_):
+			self.extractprintfile(data_)
+			self.addtobuffer("FI", self.currentfile)
+			time.sleep(4)
+		if(msglen <200 and 'T0' in data_):
+			self.extracttemp("T0:", data_)
+			self.extracttemp("T1:", data_)
+			self.extracttemp("B:", data_)
+			self.addtobuffer("T", self.temp)
+			self.check_printing()
+		if(msglen >200 and "Updated" in data_):
+			self.extractheader(data_)
+			self.addtobuffer("HD", self.header)
+		if("Stats:" in data_):
+			self.extractstats(data_)
+			self.addtobuffer("SS", self.stats)
+
+#	Check if Print was stopped
+#	If printer's status is Idle, and there is a current file and the heaters are on, change to Active
+#	If printer's status is Active, and there is a current file, and the heaters are off, change to Idle
+	def check_printing(self):
+		idle = True
+		for t in self.temp:
+			if self.temp[t][1] != 0 : idle = False
+		if(self.status == "ON" and self.currentfile != "" and not idle):
+			self.changestatus("AC")
+		elif(self.status == "AC" and self.currentfile != "" and idle):
+			print "Print Cancelled!"
+			self.changestatus("ON")
+			self.currentfile = ""
+
+#	Change the status of the printer
+#	ON: Idle/Connected, OF: Off/Disconnected, AC: Active/Printing, UM: Under Maintanence  
+	def changestatus(self, stat):
+		if(self.status != stat):
+			self.status = stat
+			self.addtobuffer("ST", stat)
+
+#	Temperature Data Sample
+#	T:140.69 /0.00' B:52.00 /0.00 T0:140.69 /0.00 T1:24.41 /0.00 @:0 B@:0 @0:0 @1:0
 	def extracttemp(self,variable,data):
 		#Extracting temperature data to a tuple in the format (Temp/SetTemp)
 		if variable in data:
@@ -32,13 +83,17 @@ class g_data:
 			if(len(temptup) > 1):
 				tup = [float(x) for x in temptup]
 				self.temp[variable[:-1]] = tup
-
+#	Header Data Sample
+#	start
+#	echo:Marlin bugfix-2.0.x
+#	echo: Last Updated: Jun 12 2019 13:30:34 | Author: (GB3, V4.x.x)
+#	echo:Compiled: Jun 12 2019
+#	echo: Free Memory: 1201  PlannerBufferBytes: 2784
 	def extractheader(self, data):
 		#Searching for last updated timestamp
 		start = end = data.find("Last Updated:") + len("Last Updated:")
 		while(ord(data[end]) != 124): end += 1
 		self.uploaddate = data[start:end].strip()
-		#print self.uploaddate
 		start = end
 		#Increment until you hit "G" for GB
 		while(ord(data[start]) != 66): start += 1
@@ -46,52 +101,25 @@ class g_data:
 		while(ord(data[end]) != 86): end += 1
 		modstring = data[start+1:end].strip()
 		self.model = "Regular" if "3" in modstring else modstring
-	
+		self.header = self.uploaddate + ".." + self.model
+#	Print File Sample
+#	echo:enqueueing "M23 thumbp~1.gco"
+#	echo:enqueueing "M24"
+#	echo:Now fresh file: thumbp~1.gco
 	def extractprintfile(self,data_):
 		start = end = data_.find(".gco") + len(".gco")
 		while(ord(data_[start]) != 32): start-=1
 		self.currentfile = data_[start:end]
 
-#  Sample Stat Data Line from Serial Read
-#  Stats: Prints: 28, Finished: 26, Failed: 2
-#  Stats: Total time: 11d 21h 42m 59s, Longest job: 5d 12h 41m 17s
-#  Stats: Filament used: 628.09m
+#	Stat Data Sample
+#	Stats: Prints: 28, Finished: 26, Failed: 2
+#	Stats: Total time: 11d 21h 42m 59s, Longest job: 5d 12h 41m 17s
+#	Stats: Filament used: 628.09m
 	def extractstats(self, data_):
 		d_list = data_.split("\n")
 		for i in range(len(d_list)):
-			d_list[i] = d_list[i].strip("Stats: ").split(",")
-			for j in range(len(d_list[i])):
-				data_component = d_list[i][j].split(":")
-				self.stats[data_component[0].strip()] = data_component[1].strip()
-
-	def check_printing(self):
-		idle = True
-		for t in self.temp:
-			if self.temp[t][1] != 0 : idle = False
-		if(self.status == "ON" and self.currentfile != "" and not idle):
-			self.changestatus("AC")
-		elif(self.status == "AC" and self.currentfile != "" and idle):
-			self.changestatus("ON")
-			self.currentfile = ""
-
-	def parsedata(self, msglen, data):
-		data_ = data.decode("utf-8")
-		if( "M23" in data_ and "M24" in data_):
-			self.extractprintfile(data_)
-			self.buffer["FI"] = self.currentfile
-			time.sleep(4)
-		if(msglen <200 and 'T0' in data_):
-			self.extracttemp("T0:", data_)
-			self.extracttemp("T1:", data_)
-			self.extracttemp("B:", data_)
-			self.buffer["T"] = self.temp
-			self.check_printing()
-			return self.temp
-		if(msglen >200 and "Updated" in data_):
-			self.extractheader(data_)
-			self.buffer["HD"] = self.uploaddate + ".." + self.model
-			return self.uploaddate + ".." + self.model
-		if("Stats:" in data_):
-			self.extractstats(data_)
-			self.buffer["SS"] = self.stats
-			return self.stats
+			if "Stat" in d_list[i]:
+				d_list[i] = d_list[i].strip("Stats: ").split(",")
+				for j in range(len(d_list[i])):
+					data_component = d_list[i][j].split(":")
+					self.stats[data_component[0].strip()] = data_component[1].strip()
