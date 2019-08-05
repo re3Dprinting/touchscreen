51ccf3789ba2fc4aafe90d7e6b570e01fbd79ff8
plloppii <noahpan323@gmail.com>
2019-08-05 13:59:58 -0700

    Control buttons added

---
+++ ClientApp/control.py
@@ -0,0 +1,37 @@
+from qt.controlwindow import *
+from PyQt5.QtCore import Qt
+
+increments = ["01", "1","10","100"]
+class ControlWindow(QtWidgets.QWidget, Ui_ControlWindow):
+	def __init__(self, parent = None):
+		super(ControlWindow, self).__init__()
+		self.setupUi(self)
+		self.parent = parent
+		self.setWindowFlags(Qt.Tool)
+
+		self.parent.setbuttonstyle(self.Back)
+		self.xbutton = self.AddButtontoGroup("x")
+		self.ybutton = self.AddButtontoGroup("y")
+		self.zbutton = self.AddButtontoGroup("z")
+		self.ebutton = self.AddButtontoGroup("e")
+		self.extruder = QtWidgets.QButtonGroup(self)
+		self.SetButtonSettings(self.E1)
+		self.SetButtonSettings(self.E2)
+		self.extruder.addButton(self.E1)
+		self.extruder.addButton(self.E2)
+
+
+	def AddButtontoGroup(self, axis):
+		group = QtWidgets.QButtonGroup(self)
+		for i in increments:
+			att = axis + "m" + i
+			self.SetButtonSettings(getattr(self,att))
+			group.addButton(getattr(self,att))
+		#group.clearCheck()
+		getattr(self, axis+ "m"+"10").setChecked(False)
+		getattr(self, axis+ "m"+"10").setChecked(True)
+		return group
+	def SetButtonSettings(self,obj):
+		obj.setCheckable(True)
+		obj.setStyleSheet("QPushButton{font: 14pt 'Ubuntu';} \
+			QPushButton:checked {background: rgba(255,255,255,0); font: 14pt 'Ubuntu'; outline: none; border: none;}")
--- ClientApp/touchdisplay.py
+++ ClientApp/touchdisplay.py
@@ -1,5 +1,6 @@
 from qt.touchdisplaywindow import *
 from PyQt5.QtCore import Qt
+from control import *
 import sys
 
 class TouchDisplay(QtWidgets.QWidget, Ui_TouchDisplay):
@@ -8,11 +9,18 @@ class TouchDisplay(QtWidgets.QWidget, Ui_TouchDisplay):
         self.setupUi(self)
         self.showFullScreen()
         self.Control.setCheckable(False)
-        self.Control.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
-        
-        self.Print.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
-        self.Settings.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
-        #self.pushButton.clicked.connect(self.printsomething)
+        #self.Control.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
+        self.setbuttonstyle(self.Print)
+        self.setbuttonstyle(self.Settings)
+        self.setbuttonstyle(self.Control)
+        #self.Print.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
+        #self.Settings.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
+        self.Control.clicked.connect(self.controlpop)
+    def controlpop(self):
+        self.con_pop = ControlWindow(self)
+        self.con_pop.show()
+    def setbuttonstyle(self,obj):
+        obj.setStyleSheet("QPushButton{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:checked{background: rgba(255,255,255,0); outline: none; border: none;} QPushButton:pressed {background: rgba(255,255,255,0); outline: none; border: none;}")
 
 if __name__ == '__main__':
 
