1225f167cce690b9524c1fc21eef58797d32b094
plloppii <noahpan323@gmail.com>
2019-08-05 11:05:19 -0700

    UI of Touch display redone

--- ClientApp/touchdisplay.py
+++ ClientApp/touchdisplay.py
@@ -6,9 +6,13 @@ class TouchDisplay(QtWidgets.QWidget, Ui_TouchDisplay):
     def __init__(self, parent = None):
         super(TouchDisplay, self).__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.printsomething)
-    def printsomething(self):
-    	self.textBrowser.append("Hello world")
+        #self.showFullScreen()
+        self.Control.setCheckable(False)
+        self.Control.setStyleSheet("QPushButton{background: transparent; outline: none;}")
+        
+        self.Print.setStyleSheet("QPushButton{background: transparent; outline: none;}");
+        self.Settings.setStyleSheet("QPushButton{background: transparent; outline: none;}");
+        #self.pushButton.clicked.connect(self.printsomething)
 
 if __name__ == '__main__':
 
