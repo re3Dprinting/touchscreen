5a985c046a03869a90eeee9cd1e53f4ebd2c162e
Jim Thompson <jim.thompson@pobox.com>
2019-11-13 13:36:09 -0600

    Checkpoint.

--- ClientApp/printwindow.py
+++ ClientApp/printwindow.py
@@ -28,7 +28,6 @@ class PrintWindow(QtWidgets.QWidget, Ui_PrintWindow):
                 self.serial.data.notprinting.connect(self.notprinting)
                 self.serial.data.printfinished.connect(self.finished)
 
-
                 self.FileList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
                 self.FileList.setSelectionMode(QtWidgets.QTableView.SingleSelection)
                 self.FileList.verticalHeader().hide()
@@ -40,6 +39,31 @@ class PrintWindow(QtWidgets.QWidget, Ui_PrintWindow):
                 self.FileList.verticalHeader().setDefaultSectionSize(50)
                 self.FileList.verticalScrollBar().setStyleSheet("QScrollBar::vertical{ width: 40px; }")
 
+
+                # Set up the list of USB files
+
+                self.USBFileList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
+                self.USBFileList.setSelectionMode(QtWidgets.QTableView.SingleSelection)
+                self.USBFileList.verticalHeader().hide()
+
+                header = self.USBFileList.horizontalHeader()
+                header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+
+                # self.USBFileList.cellActivated.connect(self.cellActivated)
+                # self.USBFileList.cellChanged.connect(self.cellChanged)
+                # self.USBFileList.cellClicked.connect(self.cellClicked)
+                # self.USBFileList.cellDoubleClicked.connect(self.cellDoubleClicked)
+                # self.USBFileList.cellEntered.connect(self.cellEntered)
+                # self.USBFileList.cellPressed.connect(self.cellPressed)
+                # self.USBFileList.currentCellChanged.connect(self.currentCellChanged)
+                self.USBFileList.currentItemChanged.connect(self.currentItemChanged)
+                # self.USBFileList.itemActivated.connect(self.itemActivated)
+                # self.USBFileList.itemChanged.connect(self.itemChanged)
+                self.USBFileList.itemClicked.connect(self.itemClicked)
+                # self.USBFileList.itemDoubleClicked.connect(self.itemDoubleClicked)
+                # self.USBFileList.itemEntered.connect(self.itemEntered)
+                # self.USBFileList.itemPressed.connect(self.itemPressed)
+                # self.USBFileList.itemSelectionChanged.connect(self.itemSelectionChanged)
 
                 tabWidth = (old_div(self.tabWidget.width(),2))-24
                 self.tabWidget.setStyleSheet(self.tabWidget.styleSheet() +"QTabBar::tab { width: " + str(tabWidth) + "px; height: 35px; font-size: 12pt;}")
@@ -140,3 +164,52 @@ class PrintWindow(QtWidgets.QWidget, Ui_PrintWindow):
 
                 # else:
                 #       self.output_serial("Please select a file")
+
+
+        # def cellActivated(self):
+        #         print("cellActivated")
+
+        # def cellChanged(self):
+        #         print("cellChanged")
+
+        # def cellClicked(self):
+        #         print("cellClicked")
+
+        # def cellDoubleClicked(self):
+        #         print("cellDoubleClicked")
+
+        # def cellEntered(self):
+        #         print("cellEntered")
+
+        # def cellPressed(self):
+        #         print("cellPressed")
+
+        # def currentCellChanged(self):
+        #         print("currentCellChanged")
+
+        def currentItemChanged(self):
+                row = self.USBFileList.currentRow()
+                print("currentItemChanged", row)
+
+        # def itemActivated(self):
+        #         print("itemActivated")
+
+        # def itemChanged(self):
+        #         print("itemChanged")
+
+        def itemClicked(self, table):
+                row = self.USBFileList.currentRow()
+                print("itemClicked ", row)
+
+        # def itemDoubleClicked(self):
+        #         print("itemDoubleClicked")
+
+        # def itemEntered(self):
+        #         print("itemEntered")
+
+        # def itemPressed(self):
+        #         print("itemPressed")
+
+        # def itemSelectionChanged(self):
+        #         print("itemSelectionChanged")
+
