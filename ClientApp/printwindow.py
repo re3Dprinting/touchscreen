4af22e27ec5867170402b60f133e574537af2949
Jim Thompson <jim.thompson@pobox.com>
2019-11-19 09:31:59 -0600

    Checkpoint

--- ClientApp/printwindow.py
+++ ClientApp/printwindow.py
@@ -42,7 +42,6 @@ class PrintWindow(QtWidgets.QWidget, Ui_PrintWindow):
         header = self.FileList.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
-        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         self.FileList.verticalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Fixed)
         self.FileList.verticalHeader().setDefaultSectionSize(50)
         self.FileList.verticalScrollBar().setStyleSheet(
