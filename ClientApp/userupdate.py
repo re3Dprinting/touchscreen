60ee45f53f592fc21ef6c7040b92935c0e7e156b
plloppii <noahpan323@gmail.com>
2019-11-30 18:27:46 -0800

    grabbing tags from repository and updating table list on userupdatewindow using PyGithub

--- ClientApp/userupdate.py
+++ ClientApp/userupdate.py
@@ -2,6 +2,7 @@ from builtins import str
 from qt.userupdatewindow import *
 from PyQt5.QtCore import Qt
 from github import Github
+from git import Repo
 
 
 class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
@@ -10,7 +11,6 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.setupUi(self)
 
 
-        
         # Make the selection Behavior as selecting the entire row
         self.SoftwareList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
         self.SoftwareList.setSelectionMode(QtWidgets.QTableView.SingleSelection)
@@ -25,6 +25,24 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
 
 
         self.Back.clicked.connect(self.close)
-        # self.CheckUpdate.clicked.connect(self.checkupdate)
+        self.CheckUpdate.clicked.connect(self.checkupdate)
         # self.Update.clicked.connect(self.update)
         # self.Rollback.clicked.connect(self.rollback)
+
+    def checkupdate(self):
+        token = "3a2f5c456fdda069efe987b19c3f6ed58c69aa37"
+        github = Github(token)
+        repo = github.get_repo("plloppii/DashboardApp")
+        tags = repo.get_tags()
+        for t in tags:
+            print(t.name, " date:", t.commit.last_modified)
+            rowpos = self.SoftwareList.rowCount()
+            self.SoftwareList.insertRow(rowpos)
+            version = QtWidgets.QTableWidgetItem(t.name)
+            version.setFlags(Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+
+            date = QtWidgets.QTableWidgetItem(t.commit.last_modified)
+            date.setFlags(Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            self.SoftwareList.setItem(rowpos,0,version)
+            self.SoftwareList.setItem(rowpos,1,date)
+
