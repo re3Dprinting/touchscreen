a2fee9784fc2ed7f640f5b1b5db05f3330246fcc
plloppii <noahpan323@gmail.com>
2019-12-01 16:02:41 -0800

    use os & gitpython to duplicate the current file, and then perform a git fetch on the copied file system. Change the bash script to boot into the new version of the software.

--- ClientApp/userupdate.py
+++ ClientApp/userupdate.py
@@ -3,6 +3,9 @@ from qt.userupdatewindow import *
 from PyQt5.QtCore import Qt
 from github import Github
 from git import Repo
+import os
+import shutil
+from pathlib import Path
 
 
 class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
@@ -11,6 +14,19 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.setupUi(self)
 
         self.app = QtWidgets.QApplication.instance()
+
+        #If the Application folder name does not have the version in it, append the version number.
+        self.old_path_tmp = Path(__file__).parents[1].__str__()
+        file_names = self.old_path_tmp.split("/")
+        if("DashboardApp" in file_names): 
+            i = file_names.index("DashboardApp")
+            file_names[i] += "_"+self.app.applicationVersion()
+            self.old_path = "/".join(file_names)
+            os.rename(self.old_path_tmp, self.old_path)
+        else:
+            self.old_path = "/".join(file_names)
+
+        self.update_path = Path(__file__).parents[2].__str__()+ "/DashboardApp_"
 
         # Make the selection Behavior as selecting the entire row
         self.SoftwareList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
@@ -29,6 +45,7 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.CheckUpdate.clicked.connect(self.checkupdate)
         self.Update.clicked.connect(self.update)
         # self.Rollback.clicked.connect(self.rollback)
+        self.editbashscript()
 
     def checkupdate(self):
         token = "3a2f5c456fdda069efe987b19c3f6ed58c69aa37"
@@ -47,12 +64,22 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
             date.setFlags(Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             self.SoftwareList.setItem(rowpos,0,version)
             self.SoftwareList.setItem(rowpos,1,date)
-            
+
     def update(self):
         software = self.SoftwareList.currentRow()
         selected_version = self.SoftwareList.item(software, 0)
-        if selected_version != None:
+        if selected_version != None and selected_version != self.app.applicationVersion():
             self.print_debug("Updating....")
+            self.update_path += selected_version
+            self.copy_and_rename()
+            update_repo = git.Repo(self.update_path)
+            update_repo.fetch()
+            update_repo.checkout(selected_version)
+            #Change Bashscript to boot up to newly pulled version.
+            editbashscript(selected_version)
+
+        elif selected_version == self.app.applicationVersion():
+            self.print_debug("Currently on that software version")
         else:
             self.print_debug("Select a Version on list")
 
@@ -61,3 +88,27 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.DebugOutput.ensureCursorVisible()
         self.DebugOutput.append(text)
 
+    def copy_and_rename(self):
+        try:
+            shutil.copytree(self.old_path,self.update_path)
+        # Directories are the same
+        except shutil.Error as e:
+            print('Directory not copied. Error: %s' % e)
+        # Any error saying that the directory doesn't exist
+        except OSError as e:
+            print('Directory not copied. Error: %s' % e)
+    def editbashscript(self, new_version):
+        script_path = Path(__file__).parents[2].__str__()
+        script_file = "/StartClientApp.sh"
+        if os.path.isfile(script_path+ script_file):
+            for line in open(script_path+script_file):
+                tmp = line.split(" ")
+                if("cd" in tmp): 
+                    tmp1 = tmp[1].split("/")
+                    i = tmp1.index("DashboardApp_"+ self.app.applicationVersion)
+                    tmp1[i] = "DashboardApp_"+ new_version
+                    tmp[1] = tmp1
+                    line = " ".join(tmp)
+                if("python" in tmp):
+                    tmp[0] = "python3"
+                    line = " ".join(tmp)
