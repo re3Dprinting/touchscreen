689a9eb78d85bb54cb6216e13be106f89611040a
plloppii <noahpan323@gmail.com>
2019-12-03 16:19:21 -0800

    Fetch tags using GitPython package

--- ClientApp/userupdate.py
+++ ClientApp/userupdate.py
@@ -1,11 +1,12 @@
 from builtins import str
 from qt.userupdatewindow import *
 from PyQt5.QtCore import Qt
-from github import Github
+# from github import Github
 from git import Repo
 import os
 import shutil
 from pathlib import Path
+import time
 
 
 class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
@@ -16,17 +17,24 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.app = QtWidgets.QApplication.instance()
 
         #If the Application folder name does not have the version in it, append the version number.
-        self.old_path_tmp = Path(__file__).parents[1].__str__()
-        file_names = self.old_path_tmp.split("/")
+        old_path_tmp = Path(__file__).parents[1].__str__()
+        self.old_path = old_path_tmp
+
+        file_names = old_path_tmp.split("/")
         if("DashboardApp" in file_names): 
             i = file_names.index("DashboardApp")
             file_names[i] += "_"+self.app.applicationVersion()
-            self.old_path = "/".join(file_names)
-            os.rename(self.old_path_tmp, self.old_path)
-        else:
-            self.old_path = "/".join(file_names)
 
+            #Only rename the file if the bashscript can be found.
+            if(self.editbashscript(self.app.applicationVersion())):
+                self.old_path = "/".join(file_names)
+                os.rename(old_path_tmp, self.old_path) 
+
+        #This will be the directory path to the new software version (Leaving the version blank initially)
         self.update_path = Path(__file__).parents[2].__str__()+ "/DashboardApp_"
+
+        print(self.old_path)
+        print(self.update_path)
 
         # Make the selection Behavior as selecting the entire row
         self.SoftwareList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
@@ -48,24 +56,40 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
 
     def checkupdate(self):
         print(Path(__file__).parents[1].__str__()+ "/git.token")
-        if(os.path.isfile(Path(__file__).parents[1].__str__()+ "/git.token")):
-            token = open(Path(__file__).parents[1].__str__()+ "/git.token").read().strip()
-            print(token)
-        else: 
-            self.print_debug("Token not found! Cannot fetch software versions")
-            return
-        github = Github(token)
-        repo = github.get_repo("plloppii/DashboardApp")
-        tags = repo.get_tags()
+        # if(os.path.isfile(Path(__file__).parents[1].__str__()+ "/git.token")):
+        #     token = open(Path(__file__).parents[1].__str__()+ "/git.token").read().strip()
+        #     print(token)
+        # else: 
+        #     self.print_debug("Token not found! Cannot fetch software versions")
+        #     return
+        # github = Github(token)
+        # repo = github.get_repo("plloppii/DashboardApp")
+        # tags = repo.get_tags()
+        g = Repo(self.old_path)
+        remote_repo = None
+        #Check if re3d remote repository is in current remote tree
+        for r in g.remotes:
+            if r.name != "re3d":
+                g.delete_remote(r)
+            else: remote_repo = r
+        #If re3d repo does not exist, add it as a remote.
+        if remote_repo == None:
+            # remote_repo = g.create_remote("re3d", "https://github.com/plloppii/DashboardApp.git")
+            remote_repo = g.create_remote("re3d", "https://github.com/re3Dprinting/touchscreen")
+        #Fetch all of the tags from the remote repository.
+        remote_repo.fetch("--tags")
+        tags = g.tags
+
         self.SoftwareList.setRowCount(0)
         for t in tags:
-            print(t.name, " date:", t.commit.last_modified)
+            tag_date = time.asctime(time.gmtime(t.commit.committed_date))
+            print(t.name, " date:", tag_date)
             rowpos = self.SoftwareList.rowCount()
             self.SoftwareList.insertRow(rowpos)
             version = QtWidgets.QTableWidgetItem(t.name)
             version.setFlags(Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
 
-            date = QtWidgets.QTableWidgetItem(t.commit.last_modified)
+            date = QtWidgets.QTableWidgetItem(tag_date)
             date.setFlags(Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             self.SoftwareList.setItem(rowpos,0,version)
             self.SoftwareList.setItem(rowpos,1,date)
@@ -102,10 +126,15 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         # Any error saying that the directory doesn't exist
         except OSError as e:
             print('Directory not copied. Error: %s' % e)
+
     def editbashscript(self, new_version):
         script_path = Path(__file__).parents[2].__str__()
         script_file = "/StartClientApp.sh"
-        if os.path.isfile(script_path+ script_file):
+        # print(script_path+script_file+ os.path.isfile(script_path+ script_file))
+        if not os.path.isfile(script_path+ script_file):
+            self.print_debug("Cannot locate script path: "+ script_path+ script_file)
+            return False
+        else:
             for line in open(script_path+script_file):
                 tmp = line.split(" ")
                 if("cd" in tmp): 
@@ -117,3 +146,4 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
                 if("python" in tmp):
                     tmp[0] = "python3"
                     line = " ".join(tmp)
+            return True
