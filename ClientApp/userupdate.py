a57c58cf7b5c4c5433884517f9558574cc191038
plloppii <noahpan323@gmail.com>
2019-12-06 13:33:26 -0800

    os.exec function working, able to restart current qt process and reboot the software.

--- ClientApp/userupdate.py
+++ ClientApp/userupdate.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3.4
+
 from builtins import str
 from qt.userupdatewindow import *
 from PyQt5.QtCore import Qt
 # from github import Github
 from git import Repo
+from git import Git
 import os
 import shutil
 from pathlib import Path
 import time
+import sys
 
 
 class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
@@ -16,25 +20,40 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
 
         self.app = QtWidgets.QApplication.instance()
 
-        #If the Application folder name does not have the version in it, append the version number.
-        old_path_tmp = Path(__file__).parents[1].__str__()
-        self.old_path = old_path_tmp
+        self.current_path = Path(__file__).parents
+        print(self.current_path[1].__str__())
+        print(self.current_path[0].__str__())
+        # #If the Application folder name does not have the version in it, append the version number.
+        # old_path_tmp = Path(__file__).parents[1].__str__()
+        # self.old_path = old_path_tmp
 
-        file_names = old_path_tmp.split("/")
-        if("DashboardApp" in file_names): 
-            i = file_names.index("DashboardApp")
-            file_names[i] += "_"+self.app.applicationVersion()
+        # file_names = old_path_tmp.split("/")
+        # if("DashboardApp" in file_names): 
+        #     i = file_names.index("DashboardApp")
+        #     file_names[i] += "_"+self.app.applicationVersion()
 
-            #Only rename the file if the bashscript can be found.
-            if(self.editbashscript(self.app.applicationVersion())):
-                self.old_path = "/".join(file_names)
-                os.rename(old_path_tmp, self.old_path) 
+        #     #Only rename the file if the bashscript can be found.
+        #     if(self.editbashscript(self.app.applicationVersion())):
+        #         self.old_path = "/".join(file_names)
+        #         os.rename(old_path_tmp, self.old_path) 
 
-        #This will be the directory path to the new software version (Leaving the version blank initially)
-        self.update_path = Path(__file__).parents[2].__str__()+ "/DashboardApp_"
+        # #This will be the directory path to the new software version (Leaving the version blank initially)
+        # self.update_path = Path(__file__).parents[2].__str__()+ "/DashboardApp_"
 
-        print(self.old_path)
-        print(self.update_path)
+        #Get the current path (local repository) and make sure that the github link is the current repository.
+        self.git = Git(self.current_path[1].__str__())
+        self.repo = Repo(self.current_path[1].__str__())
+        self.remote_repo = None
+        #Check if re3d remote repository is in current remote tree
+        for r in self.repo.remotes:
+            if r.name != "re3d":
+                self.repo.delete_remote(r)
+            else: self.remote_repo = r
+        #If re3d repo does not exist, add it as a remote.
+        if self.remote_repo == None:
+            self.remote_repo = self.repo.create_remote("re3d", "https://github.com/plloppii/DashboardApp.git")
+            # self.remote_repo = self.repo.create_remote("re3d", "https://github.com/re3Dprinting/touchscreen")
+
 
         # Make the selection Behavior as selecting the entire row
         self.SoftwareList.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
@@ -55,7 +74,6 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         # self.Rollback.clicked.connect(self.rollback)
 
     def checkupdate(self):
-        print(Path(__file__).parents[1].__str__()+ "/git.token")
         # if(os.path.isfile(Path(__file__).parents[1].__str__()+ "/git.token")):
         #     token = open(Path(__file__).parents[1].__str__()+ "/git.token").read().strip()
         #     print(token)
@@ -65,22 +83,15 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         # github = Github(token)
         # repo = github.get_repo("plloppii/DashboardApp")
         # tags = repo.get_tags()
-        g = Repo(self.old_path)
-        remote_repo = None
-        #Check if re3d remote repository is in current remote tree
-        for r in g.remotes:
-            if r.name != "re3d":
-                g.delete_remote(r)
-            else: remote_repo = r
-        #If re3d repo does not exist, add it as a remote.
-        if remote_repo == None:
-            # remote_repo = g.create_remote("re3d", "https://github.com/plloppii/DashboardApp.git")
-            remote_repo = g.create_remote("re3d", "https://github.com/re3Dprinting/touchscreen")
+        
         #Fetch all of the tags from the remote repository.
-        remote_repo.fetch("--tags")
-        tags = g.tags
+        self.remote_repo.fetch("--tags")
+        tags = self.repo.tags
 
         self.SoftwareList.setRowCount(0)
+
+        if(len(tags) == 0): 
+            self.print_debug("No software versions found. The server might be down, please try again later.")
         for t in tags:
             tag_date = time.asctime(time.gmtime(t.commit.committed_date))
             print(t.name, " date:", tag_date)
@@ -97,16 +108,13 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
     def update(self):
         software = self.SoftwareList.currentRow()
         selected_version = self.SoftwareList.item(software, 0)
-        if selected_version != None and selected_version != self.app.applicationVersion():
+        if selected_version != None and selected_version.text() != self.app.applicationVersion():
             self.print_debug("Updating....")
-            self.update_path += selected_version
-            self.copy_and_rename()
-            update_repo = git.Repo(self.update_path)
-            update_repo.fetch()
-            update_repo.checkout(selected_version)
-            #Change Bashscript to boot up to newly pulled version.
-            editbashscript(selected_version)
-
+            #checkout into new tag and run a script.
+            #script will reboot device and properly shut down Raspberry pi.
+            
+            self.git.checkout(selected_version.text())
+            os.execl(sys.executable,sys.executable,"jtmain.py")
         elif selected_version == self.app.applicationVersion():
             self.print_debug("Currently on that software version")
         else:
@@ -117,15 +125,20 @@ class UserUpdateWindow(QtWidgets.QWidget, Ui_UserUpdate):
         self.DebugOutput.ensureCursorVisible()
         self.DebugOutput.append(text)
 
-    def copy_and_rename(self):
-        try:
-            shutil.copytree(self.old_path,self.update_path)
-        # Directories are the same
-        except shutil.Error as e:
-            print('Directory not copied. Error: %s' % e)
-        # Any error saying that the directory doesn't exist
-        except OSError as e:
-            print('Directory not copied. Error: %s' % e)
+    # def copy_and_rename(self):
+    #     try:
+    #         shutil.copytree(self.old_path,self.update_path)
+    #     # Directories are the same
+    #     except shutil.Error as e:
+    #         print('Directory not copied. Error: %s' % e)
+    #     # Any error saying that the directory doesn't exist
+    #     except OSError as e:
+    #         print('Directory not copied. Error: %s' % e)
+
+
+
+
+
 
     def editbashscript(self, new_version):
         script_path = Path(__file__).parents[2].__str__()
