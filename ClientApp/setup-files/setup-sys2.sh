983008a6571e8d52d6b618b3892ae4dd2daa6cf6
Jim Thompson <jim.thompson@pobox.com>
2020-04-02 17:03:39 -0500

    Minor bugfixes.

--- ClientApp/setup-files/setup-sys2.sh
+++ ClientApp/setup-files/setup-sys2.sh
@@ -14,6 +14,7 @@ chmod +x /home/pi/StartClientApp.sh
 
 apt-get install git python3-virtualenv python3-pyqt5 fbi emacs lsof xinit xserver-xorg x11-xserver-utils x11-apps imagemagick usbmount --fix-missing -y
 
+cp usbmount.conf /etc/usbmount/usbmount.conf
 cp 00_create_model_symlink /etc/usbmount/mount.d/
 cp 00_remove_model_symlink /etc/usbmount/umount.d/
 
@@ -21,7 +22,8 @@ chown root:root /etc/usbmount/mount.d/00_create_model_symlink
 chown root:root /etc/usbmount/umount.d/00_remove_model_symlink 
 
 chown 755 /etc/usbmount/mount.d/00_create_model_symlink
-chmod 755 /etc/usbmount/umount.d/00_remove_model_symlink 
+chmod 755 /etc/usbmount/umount.d/00_remove_model_symlink
+chmod 644 /etc/usbmount/usbmount.conf
 
 cp systemd-udevd.service /lib/systemd/system/systemd-udevd.service
 chmod 644 /lib/systemd/system/systemd-udevd.service
