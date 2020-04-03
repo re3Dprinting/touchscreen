65d01fb8011764d9d4c2ac1d062eee6c884018e6
Jim Thompson <jim.thompson@pobox.com>
2020-04-03 09:52:49 -0500

    Add loop in ip.py to try getting IP for 30 seconds (while network interface comes up).

--- ClientApp/util/ip.py
+++ ClientApp/util/ip.py
@@ -1,22 +1,44 @@
 import socket
+import logging
+import time
+
+def setup_local_logger(name):
+    global logger
+    logger = logging.getLogger(name)
+
+def _log(message):
+    global logger
+    logger.debug(message)
+
+setup_local_logger(__name__)
 
 def get_ip():
     IP = 'unknown'
 
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for i in range(1, 30):
+        _log("Getting IP address, try %d of 10:" % i)
 
-        # doesn't even have to be reachable (but we'll use an IP
-        # address that we can be sure will require a route that goes
-        # through something other than localhost.
-        s.connect(('1.1.1.1', 80))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        IP = s.getsockname()[0]
+            # doesn't even have to be reachable (but we'll use an IP
+            # address that we can be sure will require a route that goes
+            # through something other than localhost.
+            s.connect(('1.1.1.1', 80))
 
-    except:
-        IP = '127.0.0.1'
+            IP = s.getsockname()[0]
 
-    finally:
-        s.close()
+            _log("Returning <%s>" % IP)
+            return IP
 
+        except:
+            _log("  OOPS: Exception occured")
+
+        finally:
+            s.close()
+
+        # Wait a second before trying again
+        time.sleep(1.0)
+
+    _log("Returning <%s>" % IP)
     return IP
