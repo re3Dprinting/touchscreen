30cddd9bfcea0d34a2b021401c2f6e47c4ed15c3
Jim Thompson <jim.thompson@pobox.com>
2020-04-09 08:12:32 -0500

    Add comments to setup scripts.

--- ClientApp/setup-files/setup-sys.sh
+++ ClientApp/setup-files/setup-sys.sh
@@ -1,21 +1,62 @@
+# Ensure that we're running as root.
 if [ `id -u` != 0 ]; then
     echo FATAL: Exected to be executed as root. Try using sudo.
     exit 1
 fi
+
+# Ensure that we're running from the setup-files directory, because
+# that's the simplest way of knowing where the files are.
 cd /home/pi/setup-files || (echo FATAL: Expected to find setup files in /home/pi/setup-files. ; exit 1)
 
+# Copy the hosts file. This establishes some host names, in particular
+# 'mac', which we can find at the other end of ethernet interface. (We
+# expect to route to the Internet through the wi-fi interface; the
+# ethernet interface is for debug/diagnostic support.)
+#
+#     raspberrypi (10.0.1.2) <-----> mac (10.0.1.1)
+#
 cp hosts /etc/hosts
+
+# Copy the interfaces file. This defines the IP address for the 'eth0'
+# interface (ethernet); and establishes that as the 10.0.1/24 network.
 cp interfaces /etc/network/interfaces
+
+# Copy the keyboard default file. This ensures we have a US keyboard
+# layout (the default for Raspbian is UK layout). It also maps the
+# CAPS LOCK key to be another control key and (optionally) sets the
+# keyboard variant to Dvorak (for Jim's CTS).
 cp keyboard /etc/default/keyboard
+
+# Copy the passwd and shadow files. This is a simple way of ensuring
+# that the 'pi' user password is what we want it to be, without having
+# to enter its plaintext into a setup file.
 cp shadow /etc/shadow
 chown 640 /etc/shadow
+
+# Copy the WPA supplicant file. This sets the wi-fi SSIDs and
+# passwords for the networks we wish to configure out of the
+# box. (Future versions will enable custom configurations of wi-fi
+# networks.
 cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Run the remainder of the script as user 'pi':
 su pi << EOF
+
+# Set up the SSH configuration, including public keys (for fast access
+via SSH without having to enter a password every time).
+
 mkdir -p /home/pi/.ssh
 cp id_rsa.pub /home/pi/.ssh/id_rsa.pub
 cp authorized_keys /home/pi/.ssh/authorized_keys
+
+# Ensure the SSH files have the right ownership and mode, or the SSH
+programs will ignore them.
+
 chown pi:pi /home/pi/.ssh
 chown pi:pi /home/pi/.ssh/id_rsa.pub
 chown pi:pi /home/pi/.ssh/authorized_keys
+
+# Copy the inputRC file so we can ignore case on filename completion.
 cp dot-inputrc /home/pi/.inputrc
+
 EOF
--- ClientApp/setup-files/setup-sys2.sh
+++ ClientApp/setup-files/setup-sys2.sh
@@ -1,18 +1,27 @@
+# Ensure that we're running as root.
 if [ `id -u` != 0 ]; then
     echo FATAL: Exected to be executed as root. Try using sudo.
     exit 1
 fi
+
+# Ensure that we're running from the setup-files directory, because
+# that's the simplest way of knowing where the files are.
 cd /home/pi/setup-files || (echo FATAL: Expected to find setup files in /home/pi/setup-files. ; exit 1)
 
+# Update Raspbian
 apt-get upgrade
 apt-get update -y
 
-mkdir -p /media/pi
-
+# Copy the script that runs the UI application and make it
+# executable.
 cp StartClientApp.sh /home/pi/StartClientApp.sh
 chmod +x /home/pi/StartClientApp.sh
 
+# Install OS packages that we require.
 apt-get install git python3-virtualenv python3-pyqt5 fbi emacs lsof xinit xserver-xorg x11-xserver-utils x11-apps imagemagick usbmount --fix-missing -y
+
+# Set up the scripts that configure the 'usbmount' package, which will
+# mount any recognized filesystem when a USB thumb drive is inserted.
 
 cp usbmount.conf /etc/usbmount/usbmount.conf
 cp 00_create_model_symlink /etc/usbmount/mount.d/
@@ -27,6 +36,9 @@ chmod 644 /etc/usbmount/usbmount.conf
 
 cp systemd-udevd.service /lib/systemd/system/systemd-udevd.service
 chmod 644 /lib/systemd/system/systemd-udevd.service
+
+# Make the directory we're going to use to contain symbolic links to
+# mounted thumb drives.
 
 mkdir /usb
 chown root:root /usb
