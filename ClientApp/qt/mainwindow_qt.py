e58f832ce0d0f18b2306af500a4f483fcbf5c959
Jim Thompson <jim.thompson@pobox.com>
2020-05-06 12:27:57 -0500

    Re-add pyuic-generated .py files.

---
+++ ClientApp/qt/mainwindow_qt.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow_qt.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 480)
+        MainWindow.setStyleSheet("")
+        self.stack = QtWidgets.QStackedWidget(MainWindow)
+        self.stack.setGeometry(QtCore.QRect(0, 0, 800, 448))
+        self.stack.setObjectName("stack")
+        self.stackedWidgetPage1 = QtWidgets.QWidget()
+        self.stackedWidgetPage1.setObjectName("stackedWidgetPage1")
+        self.stack.addWidget(self.stackedWidgetPage1)
+        self.status = QtWidgets.QWidget(MainWindow)
+        self.status.setGeometry(QtCore.QRect(0, 448, 800, 32))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.status.sizePolicy().hasHeightForWidth())
+        self.status.setSizePolicy(sizePolicy)
+        self.status.setStyleSheet("QWidget { border-top: 1px solid #ccc; }")
+        self.status.setObjectName("status")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.status)
+        self.horizontalLayout.setContentsMargins(12, 0, 12, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.left_status = QtWidgets.QLabel(self.status)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.left_status.setFont(font)
+        self.left_status.setText("")
+        self.left_status.setObjectName("left_status")
+        self.horizontalLayout.addWidget(self.left_status)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.middle_status = QtWidgets.QLabel(self.status)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.middle_status.setFont(font)
+        self.middle_status.setText("")
+        self.middle_status.setObjectName("middle_status")
+        self.horizontalLayout.addWidget(self.middle_status)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.right_status = QtWidgets.QLabel(self.status)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.right_status.setFont(font)
+        self.right_status.setText("")
+        self.right_status.setObjectName("right_status")
+        self.horizontalLayout.addWidget(self.right_status)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Form"))
---
+++ ClientApp/qt/runout.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'runout.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_w_runout_dialog(object):
+    def setupUi(self, w_runout_dialog):
+        w_runout_dialog.setObjectName("w_runout_dialog")
+        w_runout_dialog.resize(400, 240)
+        w_runout_dialog.setStyleSheet("QDialog { background-color: black; }")
+        w_runout_dialog.setModal(True)
+        self.frame = QtWidgets.QFrame(w_runout_dialog)
+        self.frame.setGeometry(QtCore.QRect(2, 2, 396, 236))
+        self.frame.setStyleSheet("QFrame { background-color: #eee; }\n"
+"")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.w_buttonBox = QtWidgets.QDialogButtonBox(self.frame)
+        self.w_buttonBox.setGeometry(QtCore.QRect(90, 180, 291, 32))
+        self.w_buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.w_buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
+        self.w_buttonBox.setObjectName("w_buttonBox")
+        self.w_runout_title = QtWidgets.QLabel(self.frame)
+        self.w_runout_title.setGeometry(QtCore.QRect(70, 20, 241, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.w_runout_title.setFont(font)
+        self.w_runout_title.setAlignment(QtCore.Qt.AlignCenter)
+        self.w_runout_title.setWordWrap(True)
+        self.w_runout_title.setObjectName("w_runout_title")
+        self.w_runout_message_label = QtWidgets.QLabel(self.frame)
+        self.w_runout_message_label.setGeometry(QtCore.QRect(50, 70, 291, 91))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.w_runout_message_label.setFont(font)
+        self.w_runout_message_label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.w_runout_message_label.setWordWrap(True)
+        self.w_runout_message_label.setObjectName("w_runout_message_label")
+
+        self.retranslateUi(w_runout_dialog)
+        QtCore.QMetaObject.connectSlotsByName(w_runout_dialog)
+
+    def retranslateUi(self, w_runout_dialog):
+        _translate = QtCore.QCoreApplication.translate
+        w_runout_dialog.setWindowTitle(_translate("w_runout_dialog", "Dialog"))
+        self.w_runout_title.setText(_translate("w_runout_dialog", "Filament Change"))
+        self.w_runout_message_label.setText(_translate("w_runout_dialog", "Filament change instructions"))
