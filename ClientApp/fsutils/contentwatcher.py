006f819bd3bd41d56ebbdfe12ff61a4ff4fbf482
plloppii <noahpan323@gmail.com>
2020-08-04 15:53:44 -0400

    Flush ContentWatcher of print statements

--- ClientApp/fsutils/contentwatcher.py
+++ ClientApp/fsutils/contentwatcher.py
@@ -6,25 +6,22 @@ class ContentWatcher(FileSystemEventHandler):
     def __init__(self, tracker):
         self.tracker = tracker
 
-    # def on_any_event(self, event):
-    #     print("Any:", event)
-
     def on_modified(self, event):
-        print("CW Modified:", event)
+        # print("CW Modified:", event)
         self.tracker.content_modified(event.src_path)
 
     def on_created(self, event):
-        print("CW Created: ", event)
+        # print("CW Created: ", event)
         self.tracker.content_modified(event.src_path)
 
     def on_deleted(self, event):
-        print("CW Deleted: ", event)
+        # print("CW Deleted: ", event)
         self.tracker.content_modified(event.src_path)
 
     def on_modified(self, event):
-        print("CW Modified: ", event)
+        # print("CW Modified: ", event)
         self.tracker.content_modified(event.src_path)
 
     def on_moved(self, event):
-        print("CW Moved: ", event)
+        # print("CW Moved: ", event)
         self.tracker.content_modified(event.src_path)
